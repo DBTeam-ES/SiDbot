75cab86340fa71d01b2882e2f71da73257ab4bc1
Tiago Danin <tiagojdanin@hotmail.com>
2015-12-15 22:19:03 +0000

    New launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-while true; do
-	lua bot.lua
-	sleep 5s
-done
+if [ "$1" = "install-debian" ]; then
+ echo "▀█▀ ▒█▀▀▀█ ▀▀█▀▀ ░█▀▀█ ▒█░░░ ▒█░░░ 
+▒█░ ░▀▀▀▄▄ ░▒█░░ ▒█▄▄█ ▒█░░░ ▒█░░░ 
+▄█▄ ▒█▄▄▄█ ░▒█░░ ▒█░▒█ ▒█▄▄█ ▒█▄▄█ 
+"
+ sudo apt-get install lua5.2 lua-sec lua-socket
+ sudo apt-get install update
+elif [ "$1" = "update" ]; then
+ echo "Baixando..."
+ git pull
+else
+ echo "▒█▀▀▀█ ▀█▀ ▒█▀▀▄ ▒█▀▀█ ▒█▀▀▀█ ▀▀█▀▀ 
+░▀▀▀▄▄ ▒█░ ▒█░▒█ ▒█▀▀▄ ▒█░░▒█ ░▒█░░ 
+▒█▄▄▄█ ▄█▄ ▒█▄▄▀ ▒█▄▄█ ▒█▄▄▄█ ░▒█░░ 
+"
+ lua bot.lua
+ sleep 5s
+fi
